4eec1083e5fbf9fe2e248842a8b98c77de44fd49
YuhoLee <57080476+YuhoLee@users.noreply.github.com>
2022-07-20 00:58:14 +0900

    Add files via upload

---
+++ Java/homework/Magazine.java
@@ -0,0 +1,38 @@
+package com.ssafy.ws05.step3;
+
+public class Magazine extends Book{
+    private int year;
+    private int month;
+    
+    public Magazine() {
+        
+    }
+    
+    public Magazine(String isbn, String title, String author, String publisher, int price, String desc, int year, int month) {
+        super(isbn, title, author, publisher, price, desc);
+        this.year = year;
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString() + "\t | " + year + "\t | " + month;
+       
+    }
+}
