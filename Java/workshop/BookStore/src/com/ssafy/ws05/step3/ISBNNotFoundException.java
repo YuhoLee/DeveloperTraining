8439a2b1fd40266df009f1f338c1fee03b86ab00
LeeYuho <cdcdefg4@gmail.com>
2022-09-13 18:48:27 +0900

    delete security file

--- Java/workshop/BookStore/src/com/ssafy/ws05/step3/ISBNNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.ws05.step3;
-
-public class ISBNNotFoundException extends Exception{
-    private static final long serialVersionUID = 1L;
-    
-    public ISBNNotFoundException() {
-        super("해당 도서가 존재하지 않습니다.");
-    }
-}
--- Java/workshop/BookStore/src/com/ssafy/ws05/step3/QuantityException.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.ws05.step3;
-
-public class QuantityException extends Exception{
-private static final long serialVersionUID = 1L;
-    
-    public QuantityException() {
-        super("수량이 부족합니다.");
-    }
-}
