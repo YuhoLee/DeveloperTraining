05ee8deee875f18b714386072a1582aae265932e
YuhoLee <cdcdefg4@inbody.com>
2024-09-20 18:51:56 +0900

    fix: Programmers - 도넛과 막대그래프

--- Algorithm/Programmers/kakao/도넛과 막대그래프.py
+++ Algorithm/Programmers/kakao/도넛과 막대그래프.py
@@ -1,6 +1,3 @@
-from collections import deque
-
-
 def solution(edges):
     # in이 없는 놈이 정점임.
     se_graph = {}
@@ -27,26 +24,28 @@ def solution(edges):
 
     res = [mp, 0, 0, 0]
     for start in se_graph[mp]:
-        visitIdxs = []
         node = start
         while True:
-            if node in visitIdxs:
-                res[1] += 1
-                break
-            else:
-                visitIdxs.append(node)
-                if node in se_graph:
-                    nextCnt = len(se_graph[node])
-                    if nextCnt == 1:
-                        node = se_graph[node][0]
-                    else:
-                        res[3] += 1
+            if node in se_graph:
+                nextCnt = len(se_graph[node])
+                if nextCnt == 1:
+                    if se_graph[node][0] == start:
+                        # 도넛
+                        res[1] += 1
                         break
+                    else:
+                        node = se_graph[node][0]
                 else:
-                    res[2] += 1
+                    # 8자
+                    res[3] += 1
                     break
 
-    print(res)
+            else:
+                # 막대
+                res[2] += 1
+                break
+
+    return res
 
 solution([[2, 3], [4, 3], [1, 1], [2, 1]])
 solution([[4, 11], [1, 12], [8, 3], [12, 7], [4, 2], [7, 11], [4, 8], [9, 6], [10, 11], [6, 10], [3, 5], [11, 1], [5, 3], [11, 9], [3, 8]])
